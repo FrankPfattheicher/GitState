fbaabeb021c7f20605f47fa5b3cf391029eaf408
Frank Pfattheicher <fpf@ict-baden.de>
2019-05-17 15:35:13 +0200

    Initial code

---
+++ GitState/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Chromely.CefGlue;
+using Chromely.Core;
+using Chromely.Core.Host;
+using IctBaden.Stonehenge3.Hosting;
+using IctBaden.Stonehenge3.Kestrel;
+using IctBaden.Stonehenge3.Resources;
+using IctBaden.Stonehenge3.Vue;
+
+namespace GitState
+{
+    internal static class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            Directory.SetCurrentDirectory(path);
+
+            // Starting stonehenge backend
+            var options = new StonehengeHostOptions
+            {
+                Title = "GitState",
+                StartPage = "main",
+                ServerPushMode = ServerPushModes.LongPolling,
+                PollIntervalMs = 30000
+            };
+            var provider = StonehengeResourceLoader
+                .CreateDefaultLoader(new VueResourceProvider());
+            var host = new KestrelHost(provider, options);
+            if (!host.Start("localhost", 0))
+            {
+                Console.WriteLine("Failed to start stonehenge server");
+            }
+
+            // Starting chromely frontend
+            Console.WriteLine("Starting chromely frontend");
+            var startUrl = host.BaseUrl;
+
+            var config = ChromelyConfiguration
+                .Create()
+                .WithLoadingCefBinariesIfNotFound(true)
+                .WithSilentCefBinariesLoading(true)
+                // ReSharper disable once RedundantArgumentDefaultValue
+                .WithHostMode(WindowState.Normal, true)
+                .WithHostTitle(options.Title)
+                .WithHostIconFile("stonehenge-chromely.ico")
+                .WithAppArgs(args)
+                .WithHostSize(100, 600)
+                .RegisterCustomerUrlScheme("http", "localhost")
+                .WithStartUrl(startUrl);
+
+            using (var window = ChromelyWindow.Create(config))
+            {
+                var exitCode = window.Run(args);
+                if (exitCode != 0)
+                {
+                    Console.WriteLine("Failed to start chromely frontend: code " + exitCode);
+                }
+            }
+            
+            Console.WriteLine("Demo done.");
+        }
+    }
+}
---
+++ GitState/ViewModels/MainVm.cs
@@ -0,0 +1,16 @@
+using IctBaden.Stonehenge3.Core;
+using IctBaden.Stonehenge3.ViewModel;
+
+namespace GitState.ViewModels
+{
+    public class MainVm : ActiveViewModel
+    {
+        public MainVm(AppSession session) : base(session)
+        {
+            
+        }
+        
+        
+        
+    }
+}
