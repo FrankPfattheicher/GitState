8dfe6fdb8c9d418af3d119dc3cc946bb552e1c7e
Frank Pfattheicher <fpf@ict-baden.de>
2023-08-15 15:29:11 +0200

    Publish unpacked

--- publish.bat
+++ publish.bat
@@ -8,10 +8,8 @@ for /F "tokens=1,2 delims= " %%a in (ReleaseNotes.md) do (
 )
 echo Version: %Version%
 
-dotnet publish -r win-x64   -c Release -f net6.0 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:AssemblyVersion=%Version% -p:Version=%Version% -p:VersionPrefix=%Version% 
-dotnet publish -r linux-x64 -c Release -f net6.0 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:AssemblyVersion=%Version% -p:Version=%Version% -p:VersionPrefix=%Version%
+dotnet publish -r win-x64   -c Release -f net6.0 -p:AssemblyVersion=%Version% -p:Version=%Version% -p:VersionPrefix=%Version% 
+dotnet publish -r linux-x64 -c Release -f net6.0 -p:AssemblyVersion=%Version% -p:Version=%Version% -p:VersionPrefix=%Version%
 
-ECHO "D" | XCopy /S /Q /Y /F "GitState\bin\Release\net6.0\win-x64\publish\GitState.exe"   "packages\GitState.exe"
-ECHO "D" | XCopy /S /Q /Y /F "GitState\bin\Release\net6.0\linux-x64\publish\GitState"     "packages\GitState"
-
-ECHO "D" | XCopy /S /Q /Y /F "GitState\GitState.cfg"  "packages\GitState.cfg"
+ECHO "V" | XCopy /S /Q /Y /F "GitState\bin\Release\net6.0\win-x64\publish"   "packages\GitState_Windows_%Version%"
+ECHO "V" | XCopy /S /Q /Y /F "GitState\bin\Release\net6.0\linux-x64\publish" "packages\GitState_Linux_%Version%"
